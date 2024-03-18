58eab0c53872a04f1f569ec70119e1dfbbb532f3
Iván <jivanrod@gmail.com>
2024-03-18 22:48:18 +0100

    Update GestorDivisasSOAP.php

--- src/modelo/GestorDivisasSOAP.php
+++ src/modelo/GestorDivisasSOAP.php
@@ -48,12 +48,18 @@ class GestorDivisasSOAP implements IGestorDivisas {
             } else if (DateTime::createFromFormat('d/m/Y', $cambiosDivisaOrigen[$i]->getFecha()) < DateTime::createFromFormat('d/m/Y', $cambiosDivisaDestino[$j]->getFecha())) {
                 $j++;
             } else {
-                if ($divisaOrigen == 2) {
+                if (($divisaOrigen == 2) && ($divisaDestino == 24)) {
+                    $venta = $cambiosDivisaDestino[$j]->getVenta();
+                    $compra = $cambiosDivisaDestino[$j]->getCompra();
+                } elseif (($divisaOrigen == 24) && ($divisaDestino == 2)) {
+                    $venta = 1 / $cambiosDivisaOrigen[$i]->getVenta();
+                    $compra = 1 / $cambiosDivisaOrigen[$i]->getCompra();
+                } elseif ($divisaOrigen == 2) {
                     $venta = $cambiosDivisaDestino[$j]->getVenta();
                     $compra = $cambiosDivisaDestino[$j]->getCompra();
                 } elseif ($divisaDestino == 2) {
-                    $venta = 1 / $cambiosDivisaDestino[$j]->getVenta();
-                    $compra = 1 / $cambiosDivisaDestino[$j]->getCompra();
+                    $venta = 1 / $cambiosDivisaOrigen[$i]->getVenta();
+                    $compra = 1 / $cambiosDivisaOrigen[$i]->getCompra();
                 } elseif ($divisaOrigen == 24) { //caso especial de euro
                     $venta = $cambiosDivisaOrigen[$i]->getVenta() * $cambiosDivisaDestino[$j]->getVenta();
                     $compra = $cambiosDivisaOrigen[$i]->getCompra() * $cambiosDivisaDestino[$j]->getCompra();
